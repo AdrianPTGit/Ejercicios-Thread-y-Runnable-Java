46cfe6937f168d71c7161b6a8954fb93a62a8db0
Adrian Pomareta <adrianpomaretagit@gmail.com>
2025-11-05 10:53:32 +0100

    Creada MyThread y su instancia en Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Runnable myRunnable =
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("Hola mundo");
-                    }
-                };
+
+       MyThread t1 = new MyThread();
+
+       t1.start();
     }
 }
